1e4b6469d2ae27271b58d37c954e280f693de886
mmm-oz <147613964+mmm-oz@users.noreply.github.com>
2023-12-09 15:24:01 +0900

    kadai_004を追加

---
+++ kadai_004/event.js
@@ -0,0 +1,7 @@
+$(function(){
+    // $('.box').on({
+    $(window).on({
+        'load': ()=>{console.log('loadイベントが発生しました')},
+        'scroll': ()=>{console.log('scrollイベントが発生しました')}
+    });
+});
